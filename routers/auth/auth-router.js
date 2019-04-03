7dab2caa1990ccc9ecb215853f724fd7dbb3ed06
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-04-03 16:10:52 -0400

    MVP complete

--- routers/auth/auth-router.js
+++ routers/auth/auth-router.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const db = require('../../data/knexConfig.js');
 const bcrypt = require('bcrypt');
 
-//const jwt = require('jsonwebtoken');
-//const secret = require('../api/secrets.js').jwtSecret;
+const jwt = require('jsonwebtoken');
+const secret = require('../../server/secrets.js').jwtSecret;
 
 
 
@@ -27,12 +27,28 @@ router.post('/login', (req,res) => {
         .first()
         .then(user => {
             if(bcrypt.compareSync(password, user.password)){
-                res.status(200).json({message: "welcome back!"})
+                const token = generateToken(user)
+                res.status(200).json({
+                    message: "welcome back!",
+                    token
+                });
             }else{
                 res.status(401).json({message: "invalid credentials"})
             }
         })
         .catch(err => res.status(500).json({errMessage: "unable to login"}))
 })
+
+function generateToken(user){
+    const payload = {
+      subject: user.id,
+      username: user.username
+    };
+    const options = {
+      expiresIn: '1h'
+    };
+    
+    return jwt.sign(payload, secret, options)
+  }
 
 module.exports = router;
--- routers/users/users-router.js
+++ routers/users/users-router.js
@@ -3,15 +3,33 @@ const router = express.Router();
 const db = require('../../data/knexConfig.js');
 const bcrypt = require('bcrypt');
 
-//const jwt = require('jsonwebtoken');
-//const secret = require('../api/secrets.js').jwtSecret;
+const jwt = require('jsonwebtoken');
+const secret = require('../../server/secrets.js').jwtSecret;
 
 
 
-router.get('/', (req,res) => {
+router.get('/', restricted, (req,res) => {
     db('users')
         .then(users => res.status(200).json(users))
         .catch(err => res.status(500).json(err))
 })
+
+function restricted(req,res,next){
+    const token = req.headers.authorization;
+
+    if(token){
+      jwt.verify(token, secret, (err, decodedToken) => {
+        if(err){
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }else{
+          req.decodedJwt = decodedToken;
+          next();
+        }
+      }) 
+    } else{
+      res.status(401).json({ message: 'Please log in' });
+    }
+  
+}
 
 module.exports = router;
